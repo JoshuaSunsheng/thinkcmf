431afbd3ca9e5a4f1278612376c6d2956b59c956
susunsheng <su371128017@163.com>
2017-04-19 15:19:19 +0800

    3.5 耐药地图调整为下拉选择省市 并实现放大缩小

--- application/Admin/Controller/ProjectController.class.php
+++ application/Admin/Controller/ProjectController.class.php
@@ -29,39 +29,33 @@ class  ProjectController extends AdminbaseController
         //利用page函数。来进行自动的分页
         $data = $db->select();
 
-//        var_dump(($data),true);
         \Think\Log::write('login write', 'WARN' . $data);
-
         $this->data = $data;
-
         $this->title = "参与成员";
-
         $this->display();
         \Think\Log::write('join end', "INFO");
 
     }
 
-    function update()
+    function joinUpdate()
     {
-        \Think\Log::write('update:', "INFO");
+        \Think\Log::write('joinUpdate:', "INFO");
 
         $db = new JoinMemberModel();
 
-        if(!empty($_POST)){
-            try{
-                if(!$db->create($_POST, 1)){
+        if (!empty($_POST)) {
+            try {
+                if (!$db->create($_POST, 1)) {
                     echo $db->getError();
-                }
-                else{
+                } else {
 
-                    if($_POST['id']){
+                    if ($_POST['id']) {
                         if (!$db->save()) {
                             echo $db->getError();
                         } else {
                             $this->success('更新成功', 'join');
                         }
-                    }
-                    else{
+                    } else {
                         if (!$db->add()) {
                             echo $db->getError();
                         } else {
@@ -70,28 +64,23 @@ class  ProjectController extends AdminbaseController
                     }
                     $this->success('成功', 'join');
                 }
-            }
-            catch(\Exception $e){ //这里的\Exception不加斜杠的话回使用think的Exception类
+            } catch (\Exception $e) { //这里的\Exception不加斜杠的话回使用think的Exception类
                 $this->error('失败', 'join');
             }
-        }
-        else{
+        } else {
             if (isset($_GET['joinMemberId'])) {
                 $this->data = $db->alias('a')
-                    ->where('a.id='.$_GET['joinMemberId'])->find();
+                    ->where('a.id=' . $_GET['joinMemberId'])->find();
 
                 $this->retCode = "00";
                 $this->msg = "查找成功";
-
-
             } else {
                 $this->retCode = "01";
                 $this->msg = "未找到该信息";
-
             }
 
             $this->display();
-            \Think\Log::write('update end', "INFO");
+            \Think\Log::write('joinUpdate end', "INFO");
         }
 
     }
@@ -100,45 +89,69 @@ class  ProjectController extends AdminbaseController
     /*
      * 医生信息
      * */
-    function form()
-    {
-        \Think\Log::write('form begin:', "INFO");
-        $db = new DoctorModel();
-
-        if (isset($_GET['doctorId'])) {
-            $this->data = $db->alias('a')->join('__DICT_TITLE__ as title ON title.id = a.title')
-                ->where('a.id='.$_GET['doctorId'])->find();
-
-            $cureTime = explode(',', $this->data["curetime"]);
-            $cureTimes = [];
-            foreach($cureTime as $ct){
-                    $cureTimes[$ct["id"]] = 'checked';
-            }
-            $this->cureTimes =$cureTimes;
-
-            $this->retCode = "00";
-            $this->msg = "查找成功";
-            $this->doctorId = $_GET['doctorId'];
-
-
-        } else {
-            $this->retCode = "01";
-            $this->msg = "未找到该信息";
-
-        }
-
-        $this->title = "医生信息";
-        $this->statuscode = $_GET['statuscode'];
-        $this->display();
-        \Think\Log::write('login form end', "INFO");
-
-    }
+//    function form()
+//    {
+//        \Think\Log::write('form begin:', "INFO");
+//        $db = new DoctorModel();
+//
+//        if (isset($_GET['doctorId'])) {
+//            $this->data = $db->alias('a')->join('__DICT_TITLE__ as title ON title.id = a.title')
+//                ->where('a.id='.$_GET['doctorId'])->find();
+//
+//            $cureTime = explode(',', $this->data["curetime"]);
+//            $cureTimes = [];
+//            foreach($cureTime as $ct){
+//                    $cureTimes[$ct["id"]] = 'checked';
+//            }
+//            $this->cureTimes =$cureTimes;
+//
+//            $this->retCode = "00";
+//            $this->msg = "查找成功";
+//            $this->doctorId = $_GET['doctorId'];
+//
+//
+//        } else {
+//            $this->retCode = "01";
+//            $this->msg = "未找到该信息";
+//
+//        }
+//
+//        $this->title = "医生信息";
+//        $this->statuscode = $_GET['statuscode'];
+//        $this->display();
+//        \Think\Log::write('login form end', "INFO");
+//
+//    }
 
 
     /*
      * 预约信息-后台显示
      * */
-    function appointment($queryStr = '', $page = 1, $pagesize = 10)
+//    function appointment($queryStr = '', $page = 1, $pagesize = 10)
+//    {
+//        \Think\Log::write('login appointment:', "INFO");
+//
+//        $DoctorController = new \Portal\Controller\DoctorController();
+//
+//        list($data, $recordnum, $pagenum) = $DoctorController->innerAppointment($queryStr, $page, $pagesize);
+//
+//        \Think\Log::write('login write', 'WARN' . $data);
+//
+//        $this->data = $data;
+//        $this->pagenum = $pagenum;
+//        $this->page = $page;
+//        $this->pagesize = $pagesize;
+//        $this->recordnum = $recordnum;
+//        $this->title = "预约信息";
+//
+//        $this->display();
+//        \Think\Log::write('login end', "INFO");
+//    }
+
+    /*
+     * 预约信息-后台显示
+     * */
+    function regionChair($queryStr = '', $page = 1, $pagesize = 10)
     {
         \Think\Log::write('login appointment:', "INFO");
 
@@ -160,6 +173,30 @@ class  ProjectController extends AdminbaseController
         \Think\Log::write('login end', "INFO");
     }
 
+    /*
+      * 预约信息-后台显示
+      * */
+    function expert($queryStr = '', $page = 1, $pagesize = 10)
+    {
+        \Think\Log::write('login appointment:', "INFO");
+
+        $DoctorController = new \Portal\Controller\DoctorController();
+
+        list($data, $recordnum, $pagenum) = $DoctorController->innerAppointment($queryStr, $page, $pagesize);
+
+//        var_dump(($data),true);
+        \Think\Log::write('login write', 'WARN' . $data);
+
+        $this->data = $data;
+        $this->pagenum = $pagenum;
+        $this->page = $page;
+        $this->pagesize = $pagesize;
+        $this->recordnum = $recordnum;
+        $this->title = "预约信息";
+
+        $this->display();
+        \Think\Log::write('login end', "INFO");
+    }
 
 
 }
